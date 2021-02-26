2125fe5e0c073d500662ea76b04606fbfa4c9f79
liurongqing <phpgege@gmail.com>
2021-02-26 11:52:25 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HelloWorld from "./demo/hello-world";
+
 import "./styles.css";
 
 export default function App() {
+  console.log("render1...");
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox 123456</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">返回首页</Link>
+            </li>
+            <li>
+              <Link to="/hello-world">hello world</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/hello-world">
+            <HelloWorld />
+          </Route>
+          <Route path="/users">
+            <HelloWorld />
+          </Route>
+          <Route path="/">
+            <HelloWorld />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
---
+++ src/demo/hello-world.tsx
@@ -0,0 +1,4 @@
+export default () => {
+  console.log("hello world render...");
+  return <h1>hello world</h1>;
+};
